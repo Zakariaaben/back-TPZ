17781a5761e74ab1dd41b42b104c5623982315b5
Zakariaaben <zakaria.benhamiche@gmail.com>
2024-05-21 18:37:27 +0100

    updating

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Importing necessary modules
 const express = require('express');
 const cors = require('cors');
+const multer = require('multer'); // Import multer for handling file uploads
 
 // Initializing express app
 const app = express();
@@ -13,24 +14,31 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Configure multer for handling file uploads
+const upload = multer({ dest: 'uploads/' }); // Destination folder for uploaded files
+
 // Initial data
 let data = "Initial Content";
 
-
-
+// GET endpoint to retrieve data
 app.get('/', (req, res) => {
   res.send('Hello World')
-})
+});
 
-// GET endpoint to retrieve data
 app.get('/data', (req, res) => {
   res.send(data);
 });
 
 // POST endpoint to update data
-app.post('/update', (req, res) => {
-  const newData = req.body.content;
-  if (newData) {
+// Accepts JSON or TXT files
+app.post('/update', upload.single('file'), (req, res) => {
+  if (req.file) {
+    // If a file is uploaded, update data with its content
+    data = req.file.buffer.toString(); // Convert buffer to string
+    res.send('Data updated successfully');
+  } else if (req.body.content) {
+    // If JSON data is provided in the request body, update data with it
+    const newData = req.body.content;
     data = newData;
     res.send('Data updated successfully');
   } else {
