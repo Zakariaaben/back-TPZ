b653e3484826fd50c006c0b5bc97796c2508d1e8
Zakariaaben <zakaria.benhamiche@gmail.com>
2024-05-21 21:44:37 +0100

    maybe

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Importing necessary modules
 const express = require('express');
 const cors = require('cors');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 
 // Initializing express app
 const app = express();
@@ -9,32 +12,43 @@ const PORT = 3002;
 // Enable CORS
 app.use(cors());
 
-// Body parsing middleware
+// Body parsing middleware for JSON
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+// Set up multer for file uploads
+const upload = multer({ dest: 'uploads/' });
 
 // Initial data
 let data = "Initial Content";
 
-
-
+// Default route
 app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+  res.send('Hello World');
+});
 
 // GET endpoint to retrieve data
 app.get('/data', (req, res) => {
   res.send(data);
 });
 
-// POST endpoint to update data
-app.post('/update', (req, res) => {
-  const newData = req.body.content;
-  if (newData) {
-    data = newData;
-    res.send('Data updated successfully');
+// POST endpoint to update data with file upload
+app.post('/update', upload.single('file'), (req, res) => {
+  if (req.file) {
+    const filePath = path.join(__dirname, req.file.path);
+    fs.readFile(filePath, 'utf8', (err, fileContent) => {
+      if (err) {
+        return res.status(500).send('Server error: Unable to read file');
+      }
+      data = fileContent;
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          return res.status(500).send('Server error: Unable to delete file');
+        }
+        res.send('Data updated successfully');
+      });
+    });
   } else {
-    res.status(400).send('Bad request: No content provided');
+    res.status(400).send('Bad request: No file provided');
   }
 });
 
@@ -42,3 +56,6 @@ app.post('/update', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+module.exports = app;
